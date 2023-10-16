24a1fc6974d1066c01a0b8e82756158fb3d1c8b7
Sarthak0085 <130341942+Sarthak0085@users.noreply.github.com>
2023-10-17 01:06:00 +0530

    Delete backend directory

--- backend/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-// authenticated user and get token
-export const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "1d"
-    });
-};
